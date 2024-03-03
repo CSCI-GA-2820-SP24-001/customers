6053542d30d0e586d0f0162736c0e58edc237624
SydneyKoby <sydneykmkoby@gmail.com>
2024-03-03 00:44:29 +0000

    Added create customer endpoint

--- service/routes.py
+++ service/routes.py
@@ -15,15 +15,15 @@
 ######################################################################
 
 """
-Pet Store Service
+Customer Store Service
 
 This service implements a REST API that allows you to Create, Read, Update
-and Delete Pets from the inventory of pets in the PetShop
+and Delete Customers from the inventory of customers in the CustomerShop
 """
 
 from flask import jsonify, request, url_for, abort
 from flask import current_app as app  # Import Flask application
-from service.models import YourResourceModel
+from service.models import Customer
 from service.common import status  # HTTP Status Codes
 
 
@@ -32,7 +32,7 @@ from service.common import status  # HTTP Status Codes
 ######################################################################
 @app.route("/")
 def index():
-    """ Root URL response """
+    """Root URL response"""
     return (
         "Reminder: return some useful information in json format about the service here",
         status.HTTP_200_OK,
@@ -43,4 +43,58 @@ def index():
 #  R E S T   A P I   E N D P O I N T S
 ######################################################################
 
-# Todo: Place your REST API code here ...
+
+######################################################################
+# CREATE A NEW CUSTOMER
+######################################################################
+@app.route("/customers", methods=["POST"])
+def create_customers():
+    """
+    Creates a Customer
+
+    This endpoint will create a Customer based the data in the body that is posted
+    """
+    app.logger.info("Request to create a customer")
+    check_content_type("application/json")
+
+    customer = Customer()
+    customer.deserialize(request.get_json())
+    customer.create()
+    message = customer.serialize()
+    # Todo: uncomment this code when get_accounts is implemented
+    # location_url = url_for("get_customers", customer_id=customer.id, _external=True)
+    location_url = "unknown"
+
+    app.logger.info("Customer with ID: %d created.", customer.id)
+    return jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
+
+
+######################################################################
+# Checks the ContentType of a request
+######################################################################
+def check_content_type(content_type):
+    """Checks that the media type is correct"""
+    if "Content-Type" not in request.headers:
+        app.logger.error("No Content-Type specified.")
+        error(
+            status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+            f"Content-Type must be {content_type}",
+        )
+
+    if request.headers["Content-Type"] == content_type:
+        return
+
+    app.logger.error("Invalid Content-Type: %s", request.headers["Content-Type"])
+    error(
+        status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+        f"Content-Type must be {content_type}",
+    )
+
+
+######################################################################
+# Logs error messages before aborting
+######################################################################
+def error(status_code, reason):
+    """Logs the error and then aborts"""
+    app.logger.error(reason)
+    abort(status_code, reason)
