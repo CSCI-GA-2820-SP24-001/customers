dec1cfeb22521a9a0ac61d9cd7428c821b99e058
aalbanese10 <aja10010@stern.nyu.edu>
2024-03-08 18:57:23 +0000

    undo get while waiting

--- service/routes.py
+++ service/routes.py
@@ -118,26 +118,3 @@ def delete_customers(customer_id):
 
     app.logger.info("Customer with ID: %d delete complete.", customer_id)
     return "", status.HTTP_204_NO_CONTENT
-
-
-######################################################################
-# READ A PET
-######################################################################
-@app.route("/customers/<int:customer_id>", methods=["GET"])
-def get_customers(customer_id):
-    """
-    Retrieve a single Customer
-
-    This endpoint will return a Customer based on it's id
-    """
-    app.logger.info("Request for customer with id: %s", customer_id)
-
-    customer = Customer.find(customer_id)
-    if not customer:
-        error(
-            status.HTTP_404_NOT_FOUND,
-            f"Customer with id '{customer_id}' was not found.",
-        )
-
-    app.logger.info("Returning customer: %s", customer.name)
-    return jsonify(customer.serialize()), status.HTTP_200_OK
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -93,6 +93,6 @@ class TestYourResourceService(TestCase):
         response = self.client.delete(f"{BASE_URL}/{test_customer.id}")
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(len(response.data), 0)
-        # make sure they are deleted
-        response = self.client.get(f"{BASE_URL}/{test_customer.id}")
-        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        # (TODO):AJ make sure they are deleted - need to do this after brian's done.
+        # response = self.client.get(f"{BASE_URL}/{test_customer.id}")
+        # self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
